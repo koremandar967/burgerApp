60452602a3ed37f3535449b902356f0d608bcbaa
unknown <koremandar967@gmail.com>
2019-12-29 23:54:43 +0530

    basic functionality completed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,16 +2,41 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary/Auxiliary'
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+const INGREDIENT_PRICES = {
+    salad : 0.5,
+    cheese : 0.4,
+    meat : 1.3,
+    bacon : 0.7
+}
+
 
 class BurgerBuilder extends Component {
 
     state = {
         ingredients : {
-            salad : 1,
-            bacon : 1,
-            cheese : 2,
-            meat : 2
-        }
+            salad : 0,
+            bacon : 0,
+            cheese : 0,
+            meat : 0
+        },
+        totalPrice : 4,
+        purchasable: false,
+        purchasing: false
+    }
+
+    updatePurchaseState(ingredients) {
+
+        const sum = Object.keys(ingredients).map(igKey => {
+            return ingredients[igKey];
+        }).reduce((sum, el) => {
+            return sum + el;
+        }, 0);
+
+        this.setState({purchasable : sum > 0});
+
     }
 
     addIngredientHandler = (type) => {
@@ -22,19 +47,81 @@ class BurgerBuilder extends Component {
 
         updateIngredients[type] = updatedCount;
 
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+
         this.setState({
-            ingredients : updateIngredients;
+            ingredients : updateIngredients,
+            totalPrice : newPrice
+        });
+        this.updatePurchaseState(updateIngredients);
+    }
+
+    removeIngredientHandler = (type) => {
+
+        const oldCount = this.state.ingredients[type];
+
+        if(oldCount <= 0) {
+            return;
+        }
+
+        const updatedCount = oldCount - 1;
+        const updateIngredients = {...this.state.ingredients}; // try this without spread op
+
+        updateIngredients[type] = updatedCount;
+
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+
+        this.setState({
+            ingredients : updateIngredients,
+            totalPrice : newPrice
         });
 
+        this.updatePurchaseState(updateIngredients);
 
+    }
+
+    purchaseHandler = () => {
+        this.setState({purchasing : true})
+    }
+
+    purchaseCancelHandler = () => {
+        this.setState({purchasing : false})
+
+    }
+
+    purchaseContinueHandler = () => {
+       alert('You continued !'); 
     }
 
     render() {
 
+        const disabledInfo = {...this.state.ingredients};
+
+        for(let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <=0 ;
+           // {salad : true , meat : false}
+        }
         return(
             <Aux>
+                <Modal show = {this.state.purchasing} modalClosed = {this.purchaseCancelHandler}>
+                    <OrderSummary 
+                    ingredients = {this.state.ingredients}
+                    price={this.state.totalPrice} 
+                    purchaseCancelled = {this.purchaseCancelHandler}
+                    purchaseContinued = {this.purchaseContinueHandler}
+                     />
+                </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
-                <BuildControls/>
+                <BuildControls ingredientAdded = {this.addIngredientHandler}
+                    ingredientRemoved = {this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    purchasable = {this.state.purchasable}
+                    ordered= {this.purchaseHandler}
+                    price={this.state.totalPrice}/>
             </Aux>
         );
     }
