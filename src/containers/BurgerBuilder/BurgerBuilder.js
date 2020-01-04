1d1b50fd136c73bbbb7ff87f211ff0446dc4a6bd
unknown <koremandar967@gmail.com>
2020-01-04 10:15:41 +0530

    firebase database added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad : 0.5,
@@ -94,7 +95,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-       alert('You continued !'); 
+
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Mandar Kore',
+                address: {
+                    street: 'balajinagar',
+                    zipcode: '411043',
+                    country: 'India'
+                },
+                email: 'koremandar967@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+
+        axios.post('/orders.json',order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
+       //alert('You continued !'); 
     }
 
     render() {
